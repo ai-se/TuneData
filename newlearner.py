3868bfedfaee14ac049dca373741bf36eb688a16
WeiFoo <wfu@ncsu.edu>
2016-03-14 18:26:24 -0400

    add threshold, which is used to tell defective or not

--- newlearner.py
+++ newlearner.py
@@ -12,21 +12,21 @@ class RF(object):
     def __init__(self):
         self.clf = self.default()
         self.tunelst = ["min_samples_split", "min_samples_leaf",
-                        "max_leaf_nodes", "n_estimators", "max_features"]
-        self.tune_min = [1, 2, 10, 50, 0.01]
-        self.tune_max = [20, 20, 50, 150, 1]
+                        "max_leaf_nodes", "n_estimators", "max_features","threshold"]
+        self.tune_min = [1, 2, 10, 50, 0.01,0.01]
+        self.tune_max = [20, 20, 50, 150, 1,1]
         self.default_value = [2, 1, None, 100, "auto", 0.5]
 
     def default(self, params=None):
         if not params:
-            return RandomForestClassifier(random_state=1)
+            return RandomForestRegressor(random_state=1)
         else:
-            return RandomForestClassifier(**params)
+            return RandomForestRegressor(**params)
 
     def tuned_parameters(self):
         return [{'n_estimators': [50, 151], 'max_features': [0.01, 1.0],
                  'min_samples_split': [1, 21], 'min_samples_leaf': [2, 21],
-                 'max_leaf_nodes': [10, 51]}]
+                 'max_leaf_nodes': [10, 51],'threshold':[0.01,1]}]
 
     def grid_parameters(self, randomly=True):
         if randomly:
@@ -34,13 +34,15 @@ class RF(object):
                  'max_features': random.sample(np.arange(0.01, 1.0, 0.1), 3),
                  'min_samples_split': random.sample(range(1, 21), 3),
                  'min_samples_leaf': random.sample(range(2, 21), 3),
-                 'max_leaf_nodes': random.sample(range(10, 51), 3)}]
+                 'max_leaf_nodes': random.sample(range(10, 51), 3),
+                 'random_state':[1]}]
         else:
             return [{'n_estimators': [50,100, 150],
                  'max_features': [0.01, 0.33, 0.99],
                  'min_samples_split': [1, 10,21],
                  'min_samples_leaf': [2, 12,21],
-                 'max_leaf_nodes': [10, 30,50]}]
+                 'max_leaf_nodes': [10, 30,50],
+                 'random_state':[1]}]
 
             # # exhuastive grid search
             # return [{'n_estimators': np.arange(50,150,10),
@@ -55,31 +57,33 @@ class CART(object):
     def __init__(self):
         self.clf = self.default()
         self.tunelst = ["max_features", "max_depth", "min_samples_split",
-                        "min_samples_leaf"]
-        self.tune_min = [0.01, 1, 2, 1]
-        self.tune_max = [1, 50, 20, 20]
+                        "min_samples_leaf","threshold"]
+        self.tune_min = [0.01, 1, 2, 1,0.1]
+        self.tune_max = [1, 50, 20, 20,1]
 
     def default(self, params=None):
         if not params:
-            return DecisionTreeClassifier(random_state=1)
+            return DecisionTreeRegressor(random_state=1)
         else:
-            return DecisionTreeClassifier(**params)
+            return DecisionTreeRegressor(**params)
 
     def tuned_parameters(self):
         return [{'max_features': [0.01, 1.0], 'max_depth': [1, 50],
-                 'min_samples_split': [1, 20], 'min_samples_leaf': [2, 21]}]
+                 'min_samples_split': [1, 20], 'min_samples_leaf': [2, 21],"threshold":[0.01,1]}]
 
     def grid_parameters(self,randomly=True):
         if randomly:
             return [{'max_features': random.sample(np.arange(0.01, 1, 0.1), 3),
                  'max_depth': random.sample(range(1, 51), 3),
                  'min_samples_split': random.sample(range(1, 20), 3),
-                 'min_samples_leaf': random.sample(range(2, 20), 3)}]
+                 'min_samples_leaf': random.sample(range(2, 20), 3),
+                 'random_state':[1]}]
         else:
             return [{'max_features': [0.01, 0.33,0.99],
                  'max_depth': [1, 25,50],
                  'min_samples_split': [1, 11,20],
-                 'min_samples_leaf': [2, 11,20]}]
+                 'min_samples_leaf': [2, 11,20],
+                 'random_state':[1]}]
             # return [{'max_features': np.arange(0.01, 1, 0.1),
             #      'max_depth': np.arange(1,51,5),
             #      'min_samples_split': np.arange(1,21,2),
