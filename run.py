2490d9c8c7e7c57e20628a3473075863fc7d19a6
WeiFoo <wfu@ncsu.edu>
2016-06-22 00:28:54 -0400

    need to add data to those tuners, not finished

--- run.py
+++ run.py
@@ -70,7 +70,8 @@ def load_data(path, num_dataset=3, data_start=3, class_col=20, cluster=False):
         df = pd.read_csv(src, header=0)
         df = df.iloc[:, data_start:]  # get rid of version,id, names
         train_Y = np.asarray(df.ix[:, class_col].as_matrix())
-        train_X = df.iloc[:, :class_col].as_matrix()  # numpy array with numeric
+        train_X = df.iloc[:,
+                  :class_col].as_matrix()  # numpy array with numeric
         return [train_X, train_Y]
 
     # folders = [f for f in listdir(path) if not isfile(join(path, f))] ### this is for local machine
@@ -89,9 +90,13 @@ def load_data(path, num_dataset=3, data_start=3, class_col=20, cluster=False):
                     X.append(build(data[i + j]))
             except IndexError, e:
                 break
-            X.append(cluster_data(data[i], data[i + 2]))  ##  put the clustered tuning data at the end of this list
-            X.append(near_data(data[i], data[i + 2]))  ##  put the nearest tuning data at the end of this list
-            X.append(kmean_data(data[i], data[i + 2]))  ## Kmeans, clustering data
+            # X.append(cluster_data(data[i], data[i + 2], isLocal=isLocal))
+            # ##  put the clustered tuning data at the end of this list
+            # X.append(near_data(data[i], data[i + 2]))
+            # ##  put the nearest tuning data at the end of this list
+            # X.append(kmean_data(data[i], data[i + 2],isLocal=isLocal))
+            # ## Kmeans, clustering data
+            # pdb.set_trace()
             yield (folder + "V" + str(count), X)
             count += 1
 
@@ -140,7 +145,27 @@ def printResult(dataname, which_is_better, lst, file_name, goal_index):
     print("\n")
 
 
-def start(src, goal, randomly=False, processor=10, repeats=20):
+def start(src, goal, randomly=False, processor=10, repeats=20, isLocal=True):
+    def help_fun(predictor, new_train_data_X, new_train_data_Y,
+                 new_tuning_data_X, new_tuning_data_Y, test_data_X,
+                 test_data_Y):
+        new_predictor = predictor()
+        for _ in xrange(repeats):
+            clf, threshold = DE_tuner(new_predictor,
+                                      goal_index=tuning_goal.index(
+                                          goal),
+                                      new_train_X=new_train_data_X,
+                                      new_train_Y=new_train_data_Y,
+                                      new_test_X=new_tuning_data_X,
+                                      new_test_Y=new_tuning_data_Y,
+                                      file_name=file_name)
+            # pdb.set_trace()
+            clf = clf.fit(new_train_data_X, new_train_data_Y)
+            predict_result = clf.predict(test_data_X)
+            score = sk_abcd(predict_result, test_data_Y,
+                            threshold=threshold)
+            save_score(name, score, score_lst)
+
     tuning_goal = ["pd", "pf", "prec", "f", "g", "auc"]
     if goal not in tuning_goal:
         raise ValueError("Tuning goal %s is not supported! only "
@@ -148,13 +173,14 @@ def start(src, goal, randomly=False, processor=10, repeats=20):
                              goal, tuple(tuning_goal)))
     # file_name = create_file(goal)
     nextpath = [src][0]  ### this is for HPC
-    file_name = create_file(nextpath[nextpath.rindex("/") + 1:] + "_" + goal)  ### this is for HPC
+    file_name = create_file(
+        nextpath[nextpath.rindex("/") + 1:] + "_" + goal)  ### this is for HPC
     which_is_better = {}
     for data_tpl in load_data(src):
         pd, pf, prec, F, g = {}, {}, {}, {}, {}
         score_lst = [pd, pf, prec, F, g]
-        data_name, data_lst = data_tpl  # data_lst include 3 consective data
-        #  sets
+        data_name, data_lst = data_tpl
+        # data_lst include 3 consective data sets
         train_data_X = np.concatenate((data_lst[0][0], data_lst[1][0]), axis=0)
         train_data_Y = np.concatenate((data_lst[0][1], data_lst[1][1]), axis=0)
         new_train_data_X = data_lst[0][0]
@@ -171,7 +197,7 @@ def start(src, goal, randomly=False, processor=10, repeats=20):
         writefile(file_name, "Dataset: " + data_name)
         for predictor in [RF, CART]:
             # for task in ["Naive_","Tuned_",  "Cluster_", "Nbrs_","Kmeans_"]:  # "Naive_", "Tuned_",
-            for task in ["Naive_","Tuned_", "Nbrs_","Kmeans_","Cluster_"]:  # "Naive_", "Tuned_",
+            for task in ["Naive_", "Tuned_"]:  # "Naive_", "Tuned_",
                 random.seed(1)
                 writefile(file_name, "-" * 30 + "\n")
                 begin_time = time.time()
@@ -209,22 +235,9 @@ def start(src, goal, randomly=False, processor=10, repeats=20):
                                         0.5)
                         save_score(name, score, score_lst)
                 elif task == "Tuned_":
-                    new_predictor = predictor()
-                    for _ in xrange(repeats):
-                        clf, threshold = DE_tuner(new_predictor,
-                                                  goal_index=tuning_goal.index(
-                                                      goal),
-                                                  new_train_X=new_train_data_X,
-                                                  new_train_Y=new_train_data_Y,
-                                                  new_test_X=new_tuning_data_X,
-                                                  new_test_Y=new_tuning_data_Y,
-                                                  file_name=file_name)
-                        # pdb.set_trace()
-                        clf = clf.fit(new_train_data_X, new_train_data_Y)
-                        predict_result = clf.predict(test_data_X)
-                        score = sk_abcd(predict_result, test_data_Y,
-                                        threshold=threshold)
-                        save_score(name, score, score_lst)
+                    help_fun(predictor, new_train_data_X, new_train_data_Y,
+                             new_tuning_data_X, new_tuning_data_Y, test_data_X,
+                             test_data_Y)
                 elif task == "Cluster_":
                     new_predictor = predictor()
                     cluster_tuning_data_X = data_lst[3][0]
